ae6082bbb8abe8e23b6b0483a1d0bf51063d8003
johanKaibotsu <kaibotsu_johan@outlook.com>
2022-03-19 15:40:27 +0530

    finished

--- desktop-notifier/desktopnotifier.py
+++ desktop-notifier/desktopnotifier.py
@@ -2,4 +2,4 @@ from socket import timeout
 from plyer import notification
 title = 'Alert'
 message = 'this is a important notification'
-notification.notify(title = title, message = message, app_icon = None, timeout = 10, toast = False)
+notification.notify(title = title, message = message, app_icon = 'desktop-notifier/icon.ico', timeout = 10)
